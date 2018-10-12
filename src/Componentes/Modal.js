0123a94558246b35b46da2f7e19369ee6c198028
valecj <valeriacossio5@gmail.com>
2018-10-12 13:26:42 -0300

    modal img

--- src/Componentes/Modal.js
+++ src/Componentes/Modal.js
@@ -12,75 +12,79 @@ import {
   Input,
   Form } from 'reactstrap';
 
-class ImgModal extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      modal: false,
-      img2: '',
-    }
-
-    this.toggle = this.toggle.bind(this);
+  export default class ImgModal extends React.Component {
+    constructor(props) {
+      super(props);
   
-  }
-
-  toggle() {
-    this.setState({
-      modal: !this.state.modal,
-    });
-  }
-
-  uploadImg2(e){
-    console.log(e)
-    this.setState({img2: e})
-  }
-
-  render(){
-    console.log("Imagenes> " + this.state.img2)
-    return (
-      <div>
-        <div className="bkgr_img" onClick={this.props.toggle}>
-        <img className="imgs" src={this.props.img} onClick={() => this.uploadImg2(this.props.img)}></img>
+      this.state = {
+        modal: false,
+        img2: '',
+      }
+    }
+  
+    toggle = () => {
+      this.setState({
+        modal: !this.state.modal,
+      });
+    }
+  
+    uploadImg2 = (img2) => {
+      this.setState({ img2 });
+      return this.toggle();
+    };
+  
+    render() {
+      const { modal, img2 } = this.state;
+      return (
+        <div>
+          <div className="bkgr_img" onClick={() => this.uploadImg2(this.props.img)}>
+            <img className="imgs" src={this.props.img} />
+          </div>
+  
+          <Modal fade={false} isOpen={modal} toggle={this.toggle} className={this.props.className}>
+  
+            <ModalHeader>
+              <Button color="secondary" className="btn_back" onClick={this.toggle}>
+                <FontAwesomeIcon className="iconArrow" icon="arrow-left" />
+                Inicio
+              </Button>
+            </ModalHeader>
+  
+            <ModalBody>
+              <Container>
+                <Row>
+                  <Col xs="2">
+                    <Button color="secondary" className="btn_puntos">
+                      <FontAwesomeIcon className="ellip_color" icon="ellipsis-h" />
+                    </Button>
+                  </Col>
+                  <Col xs="4"></Col>
+                  <Col xs="2">
+                    <Button color="secondary" className="right btn_upload">
+                      <FontAwesomeIcon icon="upload" />
+                      Enviar
+                    </Button>
+                  </Col>
+                  <Col xs="4">
+                    <Form inline>
+                      <Input type="select" name="backdrop" id="backdrop" onChange={this.changeBackdrop}>
+                        <option value="true">Imagen</option>
+                      </Input>
+                      {' '}
+                      <Button color="danger">Guardar</Button>
+                    </Form>
+                  </Col>
+                  <Col xs="6" className="mrgn_top">
+                    <img className="imgmodal" style={{ width: '100%', height: '100%' }} src={img2}/>
+                  </Col>
+                  <Col xs="6" className="mrgn_top">
+                    <p>hola</p>
+                  </Col>
+                </Row>
+              </Container>
+            </ModalBody>
+          </Modal>
         </div>
-        
-        <Modal fade={false} isOpen={this.props.isOpen} toggle={this.props.toggle} className={this.props.className}>
-        
-          <ModalHeader>
-          <Button color="secondary" className="btn_back" onClick={this.props.toggle}><FontAwesomeIcon className="iconArrow" icon="arrow-left" />  Inicio</Button>
-          </ModalHeader>
-          <ModalBody>
-            <Container>
-            <Row>
-            <Col xs="2">
-            <Button color="secondary" className="btn_puntos"><FontAwesomeIcon className="ellip_color" icon="ellipsis-h" /></Button>
-            </Col>
-            <Col xs="4"></Col>
-            <Col xs="2">
-            <Button color="secondary" className="right btn_upload"><FontAwesomeIcon icon="upload" /> Enviar</Button>
-            </Col>
-            <Col xs="4">
-            <Form inline>
-            <Input type="select" name="backdrop" id="backdrop" onChange={this.changeBackdrop}>
-              <option value="true">Imagen</option>
-            </Input>
-            {' '}
-            <Button color="danger">Guardar</Button>
-            </Form>
-            </Col>
-            <Col xs="6" className="mrgn_top">
-            <img className="imgmodal" src={this.state.img2}/>
-            </Col>
-            <Col xs="6" className="mrgn_top">
-            <p>hola</p>
-            </Col>
-            </Row>
-            </Container>
-          </ModalBody>  
-        </Modal>
-      </div>
-    );
+      );
+    }
   }
-}
-
-export default ImgModal; 
